200ec89652c2a309435ba0321c7897d68fc2a911
taras_shapik <shapiktaras@gmail.com>
2023-04-25 06:08:02 +0300

    deploy static

--- store/settings.py
+++ store/settings.py
@@ -210,11 +210,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
+else:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
-STATIC_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_URL = '/media/'
