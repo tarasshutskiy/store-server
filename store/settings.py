b9aff4ace4d913ff8e603e837e75bcf9e636c490
taras_shapik <shapiktaras@gmail.com>
2023-04-25 20:25:38 +0300

    render

--- store/settings.py
+++ store/settings.py
@@ -210,6 +210,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
