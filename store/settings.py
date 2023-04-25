e01accd8d5561eaef838b0c1c4e98fb8dce55f2c
taras_shapik <shapiktaras@gmail.com>
2023-04-25 06:36:21 +0300

    deploy static2.0

--- store/settings.py
+++ store/settings.py
@@ -209,19 +209,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = (
-        BASE_DIR / 'static',
-    )
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
