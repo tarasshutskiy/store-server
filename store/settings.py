e17e7c8d302501cb4f970dd708da77d3010f3cf5
taras_shapik <shapiktaras@gmail.com>
2023-04-25 20:48:43 +0300

    render

--- store/settings.py
+++ store/settings.py
@@ -210,15 +210,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
